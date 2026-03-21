f0c5e7d8fec93ee46d84dfe97a926b8391a2aa16
calebweixun <calebweixun@gmail.com>
2026-03-21 19:30:52 +0800

    修正編譯錯誤：移除無法識別的巨集與函數，改用標準 C 轉型處理未使用的參數

--- src/omni-projector-dock.cpp
+++ src/omni-projector-dock.cpp
@@ -1,6 +1,5 @@
 #include "omni-projector-dock.hpp"
 #include "omni-projector-manager.hpp"
-#include <obs-module.h>
 #include <obs-frontend-api.h>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
@@ -148,10 +147,10 @@ void OmniProjectorDock::RefreshMappings()
 void OmniProjectorDock::Register()
 {
 	auto cb = [](void *data, bool visible) -> QWidget * {
-		OBS_UNUSED(data);
-		OBS_UNUSED(visible);
+		(void)data;
+		(void)visible;
 		return new OmniProjectorDock();
 	};
 
-	obs_frontend_add_dock(obs_module_get_config_name(obs_current_module()), "OmniProjector", cb, nullptr);
+	obs_frontend_add_dock("OmniProjectorDock", "OmniProjector", cb, nullptr);
 }
